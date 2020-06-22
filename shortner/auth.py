ef10dc0548f88984d6c3f40799cdd9f4e4067bca
LameLemon <garybsantos@gmail.com>
2020-06-22 17:56:23 +0100

    Added field checks for signup
    - Checks password strength
    - Makes sure username and password fields are populated

--- shortner/auth.py
+++ shortner/auth.py
@@ -4,6 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
 
+import re
+
 auth = Blueprint("auth", __name__)
 
 
@@ -37,10 +39,18 @@ def signup():
 
 @auth.route("/signup", methods=["POST"])
 def signup_post():
-    # TODO ignore empty credentials, make fields required
     username = request.form.get("username")
     name = request.form.get("name")
     password = request.form.get("password")
+
+    if not (password_check(password).get("password_ok", False)):
+        flash("Your password is not strong enough, make sure it meets the following requirements:", "error")
+        flash("8 or more characters", "error")
+        flash("1 or more lowercase letters", "error")
+        flash("1 or more uppercase letters", "error")
+        flash("1 or more special characters", "error")
+        flash("1 or more digits", "error")
+        return redirect(url_for("auth.signup"))
 
     user = User.query.filter_by(username=username).first()
 
@@ -63,3 +73,45 @@ def signup_post():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+'''
+Source: https://stackoverflow.com/a/32542964/6340707
+'''
+def password_check(password):
+    """
+    Verify the strength of 'password'
+    Returns a dict indicating the wrong criteria
+    A password is considered strong if:
+        8 characters length or more
+        1 digit or more
+        1 symbol or more
+        1 uppercase letter or more
+        1 lowercase letter or more
+    """
+
+    # calculating the length
+    length_error = len(password) < 8
+
+    # searching for digits
+    digit_error = re.search(r"\d", password) is None
+
+    # searching for uppercase
+    uppercase_error = re.search(r"[A-Z]", password) is None
+
+    # searching for lowercase
+    lowercase_error = re.search(r"[a-z]", password) is None
+
+    # searching for symbols
+    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+
+    # overall result
+    password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
+
+    return {
+        'password_ok' : password_ok,
+        'length_error' : length_error,
+        'digit_error' : digit_error,
+        'uppercase_error' : uppercase_error,
+        'lowercase_error' : lowercase_error,
+        'symbol_error' : symbol_error,
+    }
