16490fe6af0793cad2c930552a9d3896c15b1cb0
LameLemon <garybsantos@gmail.com>
2020-06-23 17:50:54 +0100

    Added more nouns to GenName

--- GenName/__init__.py
+++ GenName/__init__.py
@@ -4,6 +4,7 @@ import os
 ADJECTIVES = os.path.join(os.path.dirname(__file__), 'adjective.txt')
 NOUNS = os.path.join(os.path.dirname(__file__), 'noun.txt')
 
+
 def generate():
     with open(ADJECTIVES) as f:
         adjectives = [adjective.rstrip('\n') for adjective in f]
@@ -13,4 +14,4 @@ def generate():
     w1 = random.choice(adjectives).capitalize()
     w2 = random.choice(adjectives).capitalize()
     w3 = random.choice(nouns).capitalize()
-    return "%s%s%s"%(w1, w2, w3)
+    return "%s%s%s" % (w1, w2, w3)
